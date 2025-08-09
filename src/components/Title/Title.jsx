abb95b22da49ed783781a0394da06fa8073265dc
sphirebluela <laura.sphireblue@gmail.com>
2025-08-08 20:57:09 -0700

    updated title color

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -4,7 +4,9 @@ const Title = () => {
   return (
     <div className="ma4 mt0 text-center">
       <div className="ml3">
-        <h1 className="f1 white ma0">Face Detection App</h1>
+        <h1 className="f1 ma0" style={{ color: "#4682B4" }}>
+          Face Detection App
+        </h1>
         <h2 className="f3 white ma0">Detect faces in images with ease!</h2>
       </div>
     </div>
