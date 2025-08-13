a4eec5e6989e3f72b2f8dbefb401890c1c13c267
sphirebluela <laura.sphireblue@gmail.com>
2025-08-13 15:56:47 -0700

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // ensures relative asset paths
   build: {
     outDir: "dist",
   },
