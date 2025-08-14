8771075b07912c2a7dd9a0b50cf2bb7868c8a65c
sphirebluela <laura.sphireblue@gmail.com>
2025-08-13 19:01:25 -0700

    synatx warning deactivated

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    minify: true,
+  },
   base: "./", // ensures relative asset paths
   build: {
     outDir: "dist",
+    cssMinify: false, // disables esbuild CSS minification, no warnings
   },
 });
